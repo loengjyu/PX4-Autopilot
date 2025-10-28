13735d5dee0efe0bcd7dff15c718ffd71930468d
mahima-yoga <mahima@auterion.com>
2025-10-28 11:24:37 +0100

    Address review

--- src/modules/mc_autotune_attitude_control/mc_autotune_attitude_control.cpp
+++ src/modules/mc_autotune_attitude_control/mc_autotune_attitude_control.cpp
@@ -55,10 +55,6 @@ McAutotuneAttitudeControl::~McAutotuneAttitudeControl()
 
 bool McAutotuneAttitudeControl::init()
 {
-	if (!_parameter_update_sub.registerCallback()) {
-		PX4_ERR("callback registration failed");
-		return false;
-	}
 
 	if (!_vehicle_torque_setpoint_sub.registerCallback()) {
 		PX4_ERR("callback registration failed");
