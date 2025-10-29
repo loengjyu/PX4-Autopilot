e9d2d072725f23722cbf1f48536d103b6916e896
mahima-yoga <mahima@auterion.com>
2025-10-29 18:10:30 +0100

    autotune: warn user for unused/invalid param1 in autotune command

--- src/modules/mc_autotune_attitude_control/mc_autotune_attitude_control.cpp
+++ src/modules/mc_autotune_attitude_control/mc_autotune_attitude_control.cpp
@@ -108,7 +108,16 @@ void McAutotuneAttitudeControl::Run()
 
 		if (_vehicle_command_sub.copy(&vehicle_command)) {
 			if (vehicle_command.command == vehicle_command_s::VEHICLE_CMD_DO_AUTOTUNE_ENABLE) {
-				_vehicle_cmd_start_autotune = (fabsf(vehicle_command.param1 - 1.0f) < FLT_EPSILON);
+				if (fabsf(vehicle_command.param1 - 1.0f) < FLT_EPSILON) {
+					_vehicle_cmd_start_autotune = true;
+
+				} else if (fabsf(vehicle_command.param1) < FLT_EPSILON) {
+					PX4_WARN("Disabling autotune through mavlink not supported.");
+
+				} else {
+					PX4_WARN("Invalid param1 for VEHICLE_CMD_DO_AUTOTUNE_ENABLE: %.1f (expected 0 or 1)", (double)vehicle_command.param1);
+					_vehicle_cmd_start_autotune = false; // default to safe value
+				}
 
 				if (fabsf(vehicle_command.param2) > 0.f) {
 					PX4_WARN("Axis selection not supported.");
